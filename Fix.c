9e788198778ce54b3d427f9fd60b3619ead37178
iamolku <151009655+iamolku@users.noreply.github.com>
2024-06-07 16:37:32 +0200

    Update Fix.c

--- Fix.c
+++ Fix.c
@@ -1,13 +1,35 @@
 //lil playground for me before I put these into the actual code
 
-int detected_left_sensor(pos currpos, matrix_t matrix, int orientation){
+char detected_left_sensor(pos currpos, matrix_t matrix, int orientation){
+  char c;
+  //moves slightly backward
+  stepper_steps(80,80);
 
+  //turns 10deg or so to the left
+  stepper_steps(0,-66);
 
-  return orientation;
+  //gets the color
+  c = get_c();
+
+  //turns back
+  stepper_steps(0,66);
+  
+  //moves back forward
+  stepper_steps(-80, -80);
+  return c;
 }
 
-int detected_right_sensor(pos currpos, matrix_t matrix, int orientation){
+char detected_right_sensor(pos currpos, matrix_t matrix, int orientation){
+  char c;
+  stepper_steps(80,80);
 
+  stepper_steps(-66, 0);
 
-  return orientation;
+  c = get_c();
+
+  stepper_steps(66,0);
+  
+  stepper_steps(-80, -80);
+  return c;
+
 }
