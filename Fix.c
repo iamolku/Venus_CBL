bb47ec080160459a673854e79f2fc3cd820a6cd6
iamolku <151009655+iamolku@users.noreply.github.com>
2024-06-07 16:31:24 +0200

    Update Fix.c

--- Fix.c
+++ Fix.c
@@ -1,1 +1,13 @@
+//lil playground for me before I put these into the actual code
 
+int detected_left_sensor(pos currpos, matrix_t matrix, int orientation){
+
+
+  return orientation;
+}
+
+int detected_right_sensor(pos currpos, matrix_t matrix, int orientation){
+
+
+  return orientation;
+}
